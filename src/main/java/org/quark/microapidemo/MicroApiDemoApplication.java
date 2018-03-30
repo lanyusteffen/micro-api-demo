9988f737922b95addc3410c12ace5964ff408f2d
lanyusteffen <lanyusteffen@gmail.com>
2018-03-30 17:48:27 +0800

    数据库事务支持

--- src/main/java/org/quark/microapidemo/MicroApiDemoApplication.java
+++ src/main/java/org/quark/microapidemo/MicroApiDemoApplication.java
@@ -3,12 +3,42 @@ package org.quark.microapidemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.activation.DataSource;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 @EnableKafka
-public class MicroApiDemoApplication {
+@EnableTransactionManagement(proxyTargetClass = true)
+public class MicroApiDemoApplication implements TransactionManagementConfigurer {
+
+	@Resource(name="txManager2")
+	private PlatformTransactionManager txManager2;
+
+	@Bean(name = "txManager1")
+	public PlatformTransactionManager txManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Bean(name = "txManager2")
+	public PlatformTransactionManager txManager2(EntityManagerFactory factory) {
+		return new JpaTransactionManager(factory);
+	}
+
+	// 实现接口 TransactionManagementConfigurer 方法，其返回值代表在拥有多个事务管理器的情况下默认使用的事务管理器
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return txManager2;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroApiDemoApplication.class, args);
